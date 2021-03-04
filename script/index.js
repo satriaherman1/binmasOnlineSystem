47f543a1f6d8b6877a3bb415338593a8afa5a943
satria_herman <satriaherman967@gmail.com>
2021-03-04 13:05:35 +0700

    mengubah tampilan atensi dan pusat informasi

--- script/index.js
+++ script/index.js
@@ -39,8 +39,77 @@ $(document).ready(function(){
 $('.iklan').slick({
   autoplay: true,
   slidesToShow: 1,
-  arrows : false
+  arrows : false,
+  dots: true
+
 });
+
+// infografis slick
+$('.infografis-container-item').slick({
+  autoplay : true,
+  slidesToShow: 5,
+  dots : true,
+  responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows:false
+            }
+          }
+        ]
+})
+
+// paparan slick
+$('.paparan-container-item').slick({
+  autoplay : true,
+  slidesToShow: 3,
+  dots : true,
+  responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows:false
+            }
+          }
+        ]
+})
+
 
 
 
