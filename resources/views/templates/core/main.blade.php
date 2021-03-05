11c195d538e8c2e833ef6f43b9e38fd7d3ef4bfd
satria_herman <satriaherman967@gmail.com>
2021-03-05 21:34:13 +0700

    refactoring laravel

---
+++ resources/views/templates/core/main.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet"
+     crossorigin="anonymous"> 
+    <link rel="icon" href="{{asset('img/bhabin/Binmas.svg')}}" sizes="40x40" type="image/svg">
+  
+  <!-- md bootstrap -->
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+
+  <script src="{{ asset('js/app.js') }}" ></script>
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+  <link rel="stylesheet" type="text/css"
+    href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
+     <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+   
+    <title>Binmas Online System</title>
+   
+</head>
+
+<body>
+   
+
+    @yield('mainComponent')
+
+</body>
+
+{{-- bootstrap --}}
+
+</html>
